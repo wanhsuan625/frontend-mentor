1d5aee9bd82472134ffc90f3191d2d15c7fc5dc5
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2023-09-12 16:14:16 +0800

    Style: modify disable style on arrow button

--- 8.single-page-design-portfolio/public/js/carousel.js
+++ 8.single-page-design-portfolio/public/js/carousel.js
@@ -1,10 +1,14 @@
+const arrow_prev = document.querySelector("#arrowPrev");
+const arrow_next = document.querySelector("#arrowNext");
+const img_1 = document.querySelector("#img-1");
+const img_5 = document.querySelector("#img-5");
+
 const swiper = new Swiper('.swiper', {
     
     direction: 'horizontal',
     slidesPerView: 'auto',
     centeredSlides: true,
     spaceBetween: 15,
-    // loop: true,
     initialSlide: 2,
     
     keyboard: {
@@ -16,6 +20,17 @@ const swiper = new Swiper('.swiper', {
     navigation: {
         nextEl: '.arrow-next',
         prevEl: '.arrow-prev',
+    },
+
+    // Arrow hide when image gallery goes to left/ right
+    on: {
+        slideChangeTransitionStart: function() {
+            const isImg1Active = img_1.classList.contains("swiper-slide-active");
+            const isImg5Active = img_5.classList.contains("swiper-slide-active");
+
+            arrow_prev.classList.toggle("disable", isImg1Active);
+            arrow_next.classList.toggle("disable", isImg5Active);
+        }
     },
 
     breakpoints: {
