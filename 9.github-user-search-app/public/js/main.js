bc38133876d6d19438299fc8d85a965583427857
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2023-09-27 16:57:51 +0800

    Feat: add the clearing function of search input

---
+++ 9.github-user-search-app/public/js/main.js
@@ -0,0 +1,17 @@
+const mode_container = document.querySelector("#modeContainer");
+const mode_text = document.querySelector("#modeText");
+const mode_icon = document.querySelector("#modeIcon");
+const search_input = document.querySelector("#searchInput");
+const close_icon = document.querySelector("#closeIcon");
+
+// let mode_match = window.matchMedia("( prefers-color-scheme )");
+
+// Search Bar - clear input content
+search_input.addEventListener( "input" , ()=> {
+    close_icon.style.display = search_input.value === "" ? "none" : "inline";
+})
+
+close_icon.addEventListener( "click" , ()=> {
+    search_input.value = "";
+    close_icon.style.display = "none";
+})
