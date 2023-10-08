7b8d3233fce62d6c756e53d61a1465f67270278c
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2023-10-08 14:09:43 +0800

    Feat: add search error effect

--- 9.github-user-search-app/public/js/main.js
+++ 9.github-user-search-app/public/js/main.js
@@ -2,6 +2,7 @@
 // Search bar variables
 const search_input = document.querySelector("#searchInput");
 const close_icon = document.querySelector("#closeIcon");
+const error_message = document.querySelector("#errorMessage");
 const search_button = document.querySelector("#searchButton");
 // Individual variables
 const avatar = document.querySelector("#avatar");
@@ -19,21 +20,29 @@ const website = document.querySelector("#website");
 const twitter = document.querySelector("#twitter");
 const company = document.querySelector("#company");
 
-// Search Bar ========================================
+// Search Bar ============================================
 // clear input content
 search_input.addEventListener( "input" , ()=> {
     close_icon.style.display = search_input.value === "" ? "none" : "inline";
+    error_message.classList.add("hidden");
 })
 close_icon.addEventListener( "click" , ()=> {
     search_input.value = "";
+    error_message.classList.add("hidden");
     close_icon.style.display = "none";
 })
 
-// GITHUB API ========================================
-async function fetchData(){
-    const res = await fetch(`https://api.github.com/users/kentyangfake`);
-    const data = await res.json();
-    return data;
+// GITHUB API ============================================
+async function fetchData(user){
+    const res = await fetch(`https://api.github.com/users/${user}`);
+    const getUser = await res.json();   
+
+    // user not exsits
+    if( getUser.message ){
+        error_message.classList.remove("hidden");
+        return;
+    }
+    return getUser;
 }
 
 // transform date
@@ -44,32 +53,47 @@ let transformDate = (date) => {
     return outputDate;
 }
 
-fetchData()
-.then( data => {
-    // individual information
-    avatar.src = data.avatar_url;
-    username.textContent = data.name;
-    userlink.textContent = "@" + data.login;
-    joined_date.textContent = "Joined " + transformDate(data.created_at);
+let getData = (user) => {
+    fetchData(user)
+        .then( data => {
+        // individual information
+        avatar.src = data.avatar_url;
+        username.textContent = ( data.name === null ) ? data.login : data.name;
+        userlink.textContent = "@" + data.login;
+        joined_date.textContent = "Joined " + transformDate(data.created_at);
 
-    // biography
-    bio.textContent = (data.bio === null) ? "This profile has no bio" : data.bio;
+        // biography
+        bio.textContent = (data.bio === null) ? "This profile has no bio" : data.bio;
 
-    // github repository and following situation
-    repo.textContent = data.public_repos;
-    follower.textContent = data.followers;
-    following.textContent = data.following;
+        // github repository and following situation
+        repo.textContent = data.public_repos;
+        follower.textContent = data.followers;
+        following.textContent = data.following;
 
-    // social media
-    let lack_of_information = ( variable , api_name ) => {
-        if( data[api_name] === null ){
-            variable.textContent = "No Available";
-            variable.parentElement.style.opacity = 0.3;   // 用 parentElement 將整個區域做opacity的效果
-        }else variable.textContent = data[api_name];
-    }
-    lack_of_information( site , "location" );
-    lack_of_information( website , "html_url" );
-    website.href = data.html_url;
-    lack_of_information( twitter , "twitter_username" );
-    lack_of_information( company , "company" );
-});
+        // social media
+        let lack_of_information = ( variable , api_name ) => {
+            if( data[api_name] === null ){
+                variable.textContent = "No Available";
+                variable.parentElement.style.opacity = 0.3;   // 用 parentElement 將整個區域做opacity的效果
+            }else{
+                variable.textContent = data[api_name];
+                variable.parentElement.style.opacity = 1;
+            }
+        }
+        lack_of_information( site , "location" );
+        lack_of_information( website , "html_url" );
+        website.href = data.html_url;
+        lack_of_information( twitter , "twitter_username" );
+        lack_of_information( company , "company" );
+    })
+};
+
+search_button.addEventListener( "click" , () => {
+    getData( search_input.value );
+})
+search_input.addEventListener( "keydown" , (e) => {
+    if( e.key === "Enter" ) getData( search_input.value );
+})
+
+// Default
+getData("wanhsuan625");
