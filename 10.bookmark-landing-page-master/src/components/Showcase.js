aa0a6c60cf4a706c2ca741b33d457c64aae6a95b
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2023-10-24 14:49:33 +0800

    Style: add overflow into body and html

--- 10.bookmark-landing-page-master/src/components/Showcase.js
+++ 10.bookmark-landing-page-master/src/components/Showcase.js
@@ -5,17 +5,17 @@ function Showcase(){
     return (
         <>
             <section className='flex flex-col-reverse px-8 py-20
-             lg:grid lg:grid-cols-2 lg:gap-16 lg:place-items-center max-w-7xl mx-auto'>
+             md:grid md:grid-cols-2 md:gap-16 md:place-items-center max-w-7xl mx-auto'>
             
-                <article className='text-center lg:text-left'>
-                    <h1 className='text-4xl mb-4 mt-12'>A Simple Bookmark Manager</h1>
+                <article className='text-center md:text-left'>
+                    <h1 className='text-3xl mb-4 mt-12 md:text-4xl'>A Simple Bookmark Manager</h1>
                     <p className='text-base mb-8'>
                       A clean and simple interface to organize your favourite websites.
                       Open a new browser tab and see your sites load instantly. 
                       Try it for free.
                     </p>
 
-                    <ul className='flex items-center justify-center gap-3 flex-wrap  text-sm font-medium lg:justify-start'>
+                    <ul className='flex items-center justify-center gap-3 flex-wrap  text-sm font-medium md:justify-start'>
                         <li>
                             <button className='btn-blue rounded-md py-3 px-3 text-white shadow-lg hover:opacity-75'>
                                 Get it on Chrome
@@ -30,7 +30,7 @@ function Showcase(){
                 </article>
                 
                 <article>
-                    <img src={hero} alt='' className='w-full'/>
+                    <img src={hero} alt='' className='w-full object-fill'/>
                     <div className='background'></div>
                 </article>
             </section>
