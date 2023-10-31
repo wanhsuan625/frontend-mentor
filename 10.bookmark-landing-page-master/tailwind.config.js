42c0c6fe940486d382f8fd11e6bd4d515a4eb2c5
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2023-10-31 14:29:43 +0800

    Docs: add max-width setting of input in CTA.js

--- 10.bookmark-landing-page-master/tailwind.config.js
+++ 10.bookmark-landing-page-master/tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
       colors: {
         'red_set': '#FA5959',
         'blue_set': '#5267DF',
+        'green_set': '#00BA00',
       },
+      spacing: {
+        '74': '296px',
+        '31': '124px',
+      }
     },
   },
   plugins: [],
