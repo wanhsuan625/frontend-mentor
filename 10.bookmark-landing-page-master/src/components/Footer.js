9ef7b9adc12614fe8e08285f037818dcdc31412a
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2023-11-03 09:31:43 +0800

    Feat: Add active effect on footer elements

--- 10.bookmark-landing-page-master/src/components/Footer.js
+++ 10.bookmark-landing-page-master/src/components/Footer.js
@@ -16,14 +16,14 @@ function Footer(){
 
             <ul className='text-white text-center font-normal text-sm tracking-widest py-10
                              md:flex md:gap-11 md:py-0 md:items-center md:ml-16'>
-                <li className='cursor-pointer hover:text-red_set mb-8 md:mb-0'>FEATURES</li>
-                <li className='cursor-pointer hover:text-red_set mb-8 md:mb-0'>PRICING</li>
-                <li className='cursor-pointer hover:text-red_set'>CONTACT</li>
+                <li className='cursor-pointer hover:text-red_set mb-8 md:mb-0 active:translate-y-0.5'>FEATURES</li>
+                <li className='cursor-pointer hover:text-red_set mb-8 md:mb-0 active:translate-y-0.5'>PRICING</li>
+                <li className='cursor-pointer hover:text-red_set active:translate-y-0.5'>CONTACT</li>
             </ul>
 
             <ul className='flex justify-center items-center gap-10 md:justify-end md:absolute md:right-10 xl:right-40'>
-                <Facebook className='hover:fill-red_set cursor-pointer'/>
-                <Twitter className='hover:fill-red_set cursor-pointer'/>
+                <Facebook className='cursor-pointer hover:fill-red_set  active:translate-y-0.5'/>
+                <Twitter className='cursor-pointer hover:fill-red_set  active:translate-y-0.5'/>
             </ul>
         </footer>
     </>
